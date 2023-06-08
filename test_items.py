2e3febfc5feb222564e63d5e11771621c5bc473c
forhekset1985 <forhekset1985@gmail.com>
2023-06-08 14:53:13 +0300

    init_commit. Created conftest.py and test_items.py

--- test_items.py
+++ test_items.py
@@ -5,5 +5,5 @@ class TestItem:
     def test_guest_should_see_login_link(self, browser):
         link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
-        browser.find_element(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+        assert browser.find_elements(By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket"), 'Button not found!'
         sleep(3)
